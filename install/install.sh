2ac7fce02a4bd45b05666eea89e8b62be0d4ef8d
Pavel Borisov <pavel.borisov@quercus.ru>
2014-05-26 08:31:38 +0400

    Added ruby installation to inst.script

--- install/install.sh
+++ install/install.sh
@@ -11,5 +11,11 @@ source ~/.rvm/scripts/rvm
 apt-get install -y build-essential openssl libreadline6 libreadline6-dev zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison
 # Node.js
 apt-get install -y nodejs
+# Installin ruby
+rvm install 1.9.3
+rvm use 1.9.3 --default
+rvm rubygems current
+gem install rails
+
 
 
