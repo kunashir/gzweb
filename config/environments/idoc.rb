61e490fe33f0c37621dc234dc09eb98dc96129ca
Pavel Borisov <pavel.borisov@quercus.ru>
2014-07-08 14:38:55 +0400

    Removed precompiling assets logic

--- config/environments/idoc.rb
+++ config/environments/idoc.rb
@@ -14,17 +14,18 @@ Inlook::Application.configure do
   config.action_controller.perform_caching = false
   
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  # config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
   # config.assets.js_compressor = :uglifier
 
-  config.assets.comress = false
+  # config.assets.comress = false
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # config.assets.compile = false
   # Generate digests for assets URLs.
-  config.assets.digest = true
+  # config.assets.digest = true
+  config.assets.debug = true
 
   # Specifies the header that your server uses for sending files.
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
