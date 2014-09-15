7e879238be8b4782e3f71366cfb1e47428f50e58
Pavel Borisov <pavel.borisov@quercus.ru>
2014-09-15 13:34:19 +0400

    More changes for real GZ server to work

---
+++ config/environments/gz.rb
@@ -0,0 +1,40 @@
+Inlook::Application.configure do
+  # Settings specified here will take precedence over those in config/application.rb.
+
+  # In the development environment your application's code is reloaded on
+  # every request. This slows down response time but is perfect for development
+  # since you don't have to restart the web server when you make code changes.
+  config.cache_classes = false
+
+  # Do not eager load code on boot.
+  config.eager_load = true
+
+  # Show full error reports and disable caching.
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
+  
+  # Disable Rails's static asset server (Apache or nginx will already do this).
+  # config.serve_static_assets = false
+
+  # Compress JavaScripts and CSS.
+  # config.assets.js_compressor = :uglifier
+
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  # config.assets.compile = false
+  # Generate digests for assets URLs.
+  # config.assets.digest = true
+  config.assets.debug = true
+
+  # Specifies the header that your server uses for sending files.
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+  
+  # Set to :debug to see everything in the log.
+  config.log_level = :info
+
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
+
+  # Print deprecation notices to the Rails logger.
+  config.active_support.deprecation = :log
+
+end
--- db/migrate/20140915081141_set_first_user_quick_performers.rb
+++ db/migrate/20140915081141_set_first_user_quick_performers.rb
@@ -1,11 +1,13 @@
 class SetFirstUserQuickPerformers < ActiveRecord::Migration
   def change
     u = User.first
-    u.quick_performers.destroy_all
-    index = 0
-    TakeOffice::Employee.quick_performers.each { |employee| 
-      u.quick_performers << QuickPerformer.create!(employee: employee, order: index)
-      index += 1
-    }
+    unless u.nil?
+      u.quick_performers.destroy_all
+      index = 0
+      TakeOffice::Employee.quick_performers.each { |employee| 
+        u.quick_performers << QuickPerformer.create!(employee: employee, order: index)
+        index += 1
+      }
+    end
   end
 end
