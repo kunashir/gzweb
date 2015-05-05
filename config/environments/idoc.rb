ecdd834e4f5258dcfd38ca8511ed62bcbd7d8abb
kunashir <kunashir@list.ru>
2015-05-05 19:06:43 +0300

    change idoc env2

--- config/environments/idoc.rb
+++ config/environments/idoc.rb
@@ -27,6 +27,19 @@ Inlook::Application.configure do
   # config.assets.digest = true
   config.assets.debug = false
 
+   # Disable Rails's static asset server (Apache or nginx will already do this).
+  config.serve_static_assets = false
+
+  # Compress JavaScripts and CSS.
+  config.assets.js_compressor = :uglifier
+  config.assets.css_compressor = :sass
+
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = false
+
+  # Generate digests for assets URLs.
+  config.assets.digest = true
+
   # Specifies the header that your server uses for sending files.
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
   
